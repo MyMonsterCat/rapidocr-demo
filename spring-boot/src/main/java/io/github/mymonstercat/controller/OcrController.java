0f8975b47c8d688bcefb6b6eaab5de724e5f55ec
Monster <1264833435@qq.com>
2023-11-30 17:22:17 +0800

    feat：适配0.0.6

--- spring-boot/src/main/java/io/github/mymonstercat/controller/OcrController.java
+++ spring-boot/src/main/java/io/github/mymonstercat/controller/OcrController.java
@@ -5,9 +5,11 @@ import io.github.mymonstercat.Model;
 import io.github.mymonstercat.ocr.InferenceEngine;
 import io.github.mymonstercat.ocr.config.ParamConfig;
 import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 
 /**
@@ -25,6 +27,20 @@ public class OcrController {
         InferenceEngine engine = InferenceEngine.getInstance(Model.ONNX_PPOCR_V3);
         // 开始识别
         OcrResult ocrResult = engine.runOcr(getResourcePath("images/test.png"), paramConfig);
+        return ocrResult.getStrRes().toString();
+    }
+
+    @PostMapping
+    public String ocr(@RequestParam("file") MultipartFile fileUpload) throws IOException {
+        ParamConfig paramConfig = ParamConfig.getDefaultConfig();
+        paramConfig.setDoAngle(true);
+        paramConfig.setMostAngle(true);
+        InferenceEngine engine = InferenceEngine.getInstance(Model.ONNX_PPOCR_V3);
+        File file = new File(System.getProperty("java.io.tmpdir") + "ocrJava/test.png");
+        fileUpload.transferTo(file);
+        file.deleteOnExit();
+
+        OcrResult ocrResult = engine.runOcr(file.getPath(),paramConfig);
         return ocrResult.getStrRes().toString();
     }
 
